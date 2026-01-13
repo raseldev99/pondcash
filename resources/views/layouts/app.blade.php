6a067005e0134deeb3d697b7eb39189959d0d42e
raseldev99 <raselbabu34.bd@gmail.com>
2026-01-13 16:44:18 +0600

    fix the captcha problem

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -42,20 +42,20 @@
         @vite(['resources/assets/css/app.css'])
         @stack('css')
         <!-- Google tag (gtag.js) -->
-{{--        @if(setting('services.google.analytics_enable'))--}}
-{{--            <script async defer--}}
-{{--                    src="https://www.googletagmanager.com/gtag/js?id={{ setting('services.google.analytics_id') }}"></script>--}}
-{{--            <script>--}}
-{{--                window.dataLayer = window.dataLayer || [];--}}
+       @if(setting('services.google.analytics_enable'))
+           <script async defer
+                   src="https://www.googletagmanager.com/gtag/js?id={{ setting('services.google.analytics_id') }}"></script>
+           <script>
+               window.dataLayer = window.dataLayer || [];
 
-{{--                function gtag() {--}}
-{{--                    dataLayer.push(arguments);--}}
-{{--                }--}}
+                function gtag() {
+                   dataLayer.push(arguments);
+                }
 
-{{--                gtag('js', new Date());--}}
-{{--                gtag('config', '{{ setting('services.google.analytics_id') }}');--}}
-{{--            </script>--}}
-{{--        @endif--}}
+                gtag('js', new Date());
+                gtag('config', '{{ setting('services.google.analytics_id') }}');
+            </script>
+        @endif 
 
        <script>
            window.recaptchaSiteKey = "{{ config('recaptcha.api_site_key') }}";
@@ -141,22 +141,9 @@
        </script>
 
        <script src="https://www.google.com/recaptcha/api.js?onload=onRecaptchaLoad&render=explicit" async defer></script>
-
-        <!-- Google Tag Manager -->
-        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-NNGSXD2R');</script>
-        <!-- End Google Tag Manager -->
-
     </head>
 
     <body>
-    <!-- Google Tag Manager (noscript) -->
-    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NNGSXD2R"
-                      height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-    <!-- End Google Tag Manager (noscript) -->
         <div id="preloader">
             <div class="text-center no-hover">
                 <x-logo class="mx-auto"/>
